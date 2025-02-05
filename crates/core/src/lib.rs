9846cc2ed2d526d4e5ce0ea610bac9132942ea7f
Juanperias <136520331+Juanperias@users.noreply.github.com>
2025-02-05 12:35:16 -0400

    refactor: make some memory addr checks before write/read

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -7,14 +7,21 @@ pub fn run_test<M: Mem, L: Logger>(logger: &L, mem: &M, region: MemoryRegion) {
 
     logger.log(format_args!("Checking region {:?}", region));
     let offset_region = mem.parse(region);
+    let mut bads = 0;
     for addr in offset_region.start..offset_region.end {
+        if !mem.check(addr) {
+            bads += 1;
+            continue;
+        }        
+
         mem.write(addr, 2);
 
         if mem.read(addr) != 2 {
             panic!("Oh no!");
         }
-       
     }
+    logger.log(format_args!("{}", bads));
+    for _i in 0..10_000 {}
 }
 
 #[derive(Debug)]
@@ -25,6 +32,7 @@ pub struct MemoryRegion {
 
 pub trait Mem {
     fn parse(&self, region: MemoryRegion) -> MemoryRegion;
+    fn check(&self, addr: u64) -> bool;
     fn read(&self, addr: u64) -> u64;
     fn write(&self, addr: u64, value: u64);
 }
--- kernel/src/mem/mod.rs
+++ kernel/src/mem/mod.rs
@@ -1,6 +1,8 @@
 use core::sync::atomic::{AtomicU64, Ordering};
 use memsos_core::Mem;
 
+const ALIGNMENT: usize = core::mem::align_of::<u64>();
+
 #[derive(Debug)]
 pub struct MemWriter {
     offset: AtomicU64,
@@ -8,7 +10,7 @@ pub struct MemWriter {
 
 impl Mem for MemWriter {
     fn read(&self, addr: u64) -> u64 {
-        let ptr = addr as *const u64;
+        let ptr = addr as *mut u64;
 
         unsafe {
             ptr.read()
@@ -18,8 +20,12 @@ impl Mem for MemWriter {
         let ptr = addr as *mut u64;
 
         unsafe {
-            *ptr = value;
+            ptr.write_volatile(value);
         }
+    }
+    fn check(&self, addr: u64) -> bool {
+       let ptr = addr as *mut u64;
+       addr as usize % ALIGNMENT == 0 && !ptr.is_null()
     }
     fn parse(&self, region: memsos_core::MemoryRegion) -> memsos_core::MemoryRegion {
         let offset = self.offset.load(Ordering::SeqCst);
