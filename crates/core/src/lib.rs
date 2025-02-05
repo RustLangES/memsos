4dda6a10dd1d7d222322b49139c37ff047e97226
Juanperias <136520331+Juanperias@users.noreply.github.com>
2025-02-05 16:33:12 -0400

    feat: implement march-C algorithm

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -3,22 +3,45 @@
 use core::fmt::Arguments; 
 
 pub fn run_test<M: Mem, L: Logger>(logger: &L, mem: &M, region: MemoryRegion) {
-    // Simulating a test this should be a real test in the future
-
     logger.log(format_args!("Checking region {:?}", region));
     let offset_region = mem.parse(region);
 
     for addr in offset_region.start..offset_region.end {
         if !mem.check(addr) {
             continue;
-        }        
-
-        mem.write(addr, 2);
-
-        if mem.read(addr) != 2 {
-            panic!("Oh no!");
         }
+        mem.write(addr, 0);
     }
+
+    for addr in offset_region.start..offset_region.end {
+        if !mem.check(addr) {
+            continue;
+        }
+        if mem.read(addr) != 0 {
+            panic!("Test failed at address 1 {:?}", addr);
+        }
+        mem.write(addr, 1);
+    }
+
+    for addr in (offset_region.start..offset_region.end).rev() {
+        if !mem.check(addr) {
+            continue;
+        }
+        if mem.read(addr) != 1 {
+            panic!("Test failed at address 2 {:?}", addr);
+        }
+        mem.write(addr, 0);
+    }
+
+    for addr in offset_region.start..offset_region.end {
+        if !mem.check(addr) {
+            continue;
+        }
+        if mem.read(addr) != 0 {
+            panic!("Test failed at address 3 {:?}", addr);
+        }
+        mem.write(addr, 1);
+    } 
 }
 
 #[derive(Debug)]
