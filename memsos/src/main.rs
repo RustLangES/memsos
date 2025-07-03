2a877e0428c3ac7db0d2b40d67b0a9027a4699da
juanperias <juanperiasyt@gmail.com>
2025-07-03 12:50:09 -0400

    feat: detect gop and get the framebuffer

--- memsos/src/main.rs
+++ memsos/src/main.rs
@@ -6,7 +6,7 @@ extern crate alloc;
 mod once;
 
 use alloc::{string::String, vec::Vec};
-use r_efi::efi::{self, Char16};
+use r_efi::{efi::{self, Char16}, protocols::graphics_output::BltPixel};
 use core::fmt::Write;
 use once::Once;
 
@@ -43,6 +43,85 @@ macro_rules! println {
     }
 }
 
+
+// https://github.com/r-efi/r-efi/blob/main/examples/gop-query.rs
+fn locate_singleton(
+    st: *mut efi::SystemTable,
+    guid: *const efi::Guid,
+) -> Result<*mut core::ffi::c_void, efi::Status> {
+    let mut interface: *mut core::ffi::c_void = core::ptr::null_mut();
+    let mut handles: *mut efi::Handle = core::ptr::null_mut();
+    let mut n_handles: usize = 0;
+    let mut r: efi::Status;
+
+    unsafe {
+        if (*st).hdr.revision < efi::SYSTEM_TABLE_REVISION_1_10 {
+            return Err(efi::Status::UNSUPPORTED);
+        }
+
+        let r = ((*(*st).boot_services).locate_handle_buffer)(
+            efi::BY_PROTOCOL,
+            guid as *mut _,
+            core::ptr::null_mut(),
+            &mut n_handles,
+            &mut handles,
+        );
+        match r {
+            efi::Status::SUCCESS => {}
+            efi::Status::NOT_FOUND => return Err(r),
+            efi::Status::OUT_OF_RESOURCES => return Err(r),
+            _ => panic!(),
+        };
+    }
+
+    unsafe {
+        r = efi::Status::NOT_FOUND;
+        for i in 0..n_handles {
+            r = ((*(*st).boot_services).handle_protocol)(
+                *handles.offset(core::convert::TryFrom::<usize>::try_from(i).unwrap()),
+                guid as *mut _,
+                &mut interface,
+            );
+            match r {
+                efi::Status::SUCCESS => break,
+                efi::Status::UNSUPPORTED => continue,
+                _ => panic!(),
+            };
+        }
+    }
+
+    unsafe {
+        let r = ((*(*st).boot_services).free_pool)(handles as *mut core::ffi::c_void);
+        assert!(!r.is_error());
+    }
+    match r {
+        efi::Status::SUCCESS => Ok(interface),
+        _ => Err(efi::Status::NOT_FOUND),
+    }
+}
+
+fn query_gop(
+    gop: *mut efi::protocols::graphics_output::Protocol,
+) -> Result<(u32, u32), efi::Status> {
+    let mut info: *mut efi::protocols::graphics_output::ModeInformation = core::ptr::null_mut();
+    let mut z_info: usize = 0;
+
+    unsafe {
+        let r = ((*gop).query_mode)(gop, (*(*gop).mode).mode, &mut z_info, &mut info);
+        match r {
+            efi::Status::SUCCESS => {}
+            efi::Status::DEVICE_ERROR => return Err(r),
+            _ => panic!(),
+        };
+        if z_info < core::mem::size_of_val(&*info) {
+            return Err(efi::Status::UNSUPPORTED);
+        }
+
+        Ok(((*info).horizontal_resolution, (*info).vertical_resolution))
+    }
+}
+
+
 #[unsafe(no_mangle)]
 pub extern "C" fn efi_main(_h: efi::Handle, st: *mut efi::SystemTable) -> efi::Status {
     SYSTEM_TABLE.call_once(|| st);
@@ -55,20 +134,13 @@ pub extern "C" fn efi_main(_h: efi::Handle, st: *mut efi::SystemTable) -> efi::S
     efi_run(_h, st)
 }
 
+
+
 fn efi_run(_h: efi::Handle, st: *mut efi::SystemTable) -> efi::Status {
-
-    let s = String::from("Hello from memsos!");
-    
-    let mut v: Vec<u16> = s.encode_utf16().collect();
-    v.push(0);
-    unsafe {
-        ((*(*st).con_out).set_attribute)((*st).con_out, EFI_WHITE | (EFI_RED << 4));
-
-        ((*(*st).con_out).clear_screen)((*st).con_out);
-         
-//        panic!();
-//
-    }
+    let gop = locate_singleton(st, &efi::protocols::graphics_output::PROTOCOL_GUID).unwrap() as *mut efi::protocols::graphics_output::Protocol;
+    let mode = unsafe { *((*gop).mode) };
+    let framebuffer =  unsafe { core::slice::from_raw_parts_mut(mode.frame_buffer_base as *mut u8, mode.frame_buffer_size) };
+    framebuffer.fill(0xFF);
 
     loop {}
 
