1007451979fb9758e84e8883bb2bdeb1be0e590a
juanperias <juanperiasyt@gmail.com>
2025-07-19 20:15:40 -0400

    feat: add fb, boot crates

---
+++ crates/boot/src/requests.rs
@@ -0,0 +1,19 @@
+use limine::request::{
+    ExecutableAddressRequest, FramebufferRequest, HhdmRequest, MemoryMapRequest,
+};
+
+#[used]
+#[unsafe(link_section = ".requests")]
+pub static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+pub static HHDM_REQUEST: HhdmRequest = HhdmRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+pub static KERNEL_ADDRESS: ExecutableAddressRequest = ExecutableAddressRequest::new();
+
+#[used]
+#[unsafe(link_section = ".requests")]
+pub static MEMORY_MAP_REQUEST: MemoryMapRequest = MemoryMapRequest::new();
