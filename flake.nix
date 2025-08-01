f42b603309705c3ca8a1f18eaf1968ad1acbbff7
Sergio Alejandro Ribera Costa <56278796+SergioRibera@users.noreply.github.com>
2025-08-01 14:49:49 -0400

    separate strategies in other file

--- flake.nix
+++ flake.nix
@@ -54,29 +54,7 @@
         langFiles = builtins.attrNames (lib.filterAttrs (name: _: lib.hasSuffix ".json" name) (builtins.readDir ./crates/lang/defs));
         languages = map (file: lib.removeSuffix ".UTF-8.json" file) langFiles;
 
-        architectures = [
-          {
-            arch = "x86_64";
-            name = "x86_64";
-            target = "x86_64-unknown-none";
-          }
-          {
-            arch = "aarch64";
-            name = "aarch64";
-            target = "aarch64-unknown-none";
-          }
-          # { arch = "aarch64"; name = "aarch64-uefi";  target = "aarch64-unknown-uefi"; }
-          {
-            arch = "riscv64";
-            name = "riscv64-imac";
-            target = "riscv64imac-unknown-none-elf";
-          }
-          {
-            arch = "riscv64";
-            name = "riscv64-gc";
-            target = "riscv64gc-unknown-none-elf";
-          }
-        ];
+        architectures = import ./strategies.nix;
 
         mkDevShell = {
           name,
---
+++ strategies.nix
@@ -0,0 +1,7 @@
+[
+    { arch = "x86_64"; name = "x86_64"; target = "x86_64-unknown-none"; }
+    # { arch = "aarch64"; name = "aarch64"; target = "aarch64-unknown-none"; }
+    # { arch = "aarch64"; name = "aarch64-uefi";  target = "aarch64-unknown-uefi"; }
+    # { arch = "riscv64"; name = "riscv64-imac"; target = "riscv64imac-unknown-none-elf"; }
+    # { arch = "riscv64"; name = "riscv64-gc"; target = "riscv64gc-unknown-none-elf"; }
+]
