13e3e1c51291f40b1657f1a3f7f70df502f105d2
Juanperias <juanperiasyt@gmail.com>
2025-08-07 14:03:46 -0400

    feat: write_context naked fn

--- kernel/src/scheduler.rs
+++ kernel/src/scheduler.rs
@@ -77,6 +77,28 @@ pub extern "C" fn fill_context(ctx: *mut Context) {
     );
 }
 
+#[unsafe(naked)]
+pub extern "C" fn write_context(ctx: *const Context) {
+    core::arch::naked_asm!(
+        "mov rax, [rdi + 0x00]",
+        "mov rbx, [rdi + 0x08]",
+        "mov rcx, [rdi + 0x10]",
+        "mov rdx, [rdi + 0x18]",
+        "mov rsi, [rdi + 0x20]",
+        "mov rbp, [rdi + 0x30]",
+        "mov rsp, [rdi + 0x38]",
+        "mov r8, [rdi + 0x40]",
+        "mov r9, [rdi + 0x48]",
+        "mov r10, [rdi + 0x50]",
+        "mov r10, [rdi + 0x58]",
+        "mov r12, [rdi + 0x60]",
+        "mov r13, [rdi + 0x68]",
+        "mov r14, [rdi + 0x70]",
+        "mov r15, [rdi + 0x78]",
+        "jmp [rdi + 0x80]",
+    );
+}
+
 impl Process {
     pub fn new(start: VirtAddr) -> Self {
         let mut ctx = Context::default();
@@ -97,9 +119,7 @@ impl Process {
         }
     }
     pub fn run(&self) {
-        unsafe {
-            asm!("jmp {}", in(reg) self.context.rip);
-        }
+        write_context((&self.context) as *const Context);
     }
     pub fn stop(&mut self, rip: VirtAddr) {
         //self.context.rip = rip;
